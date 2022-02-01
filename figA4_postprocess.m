b697e9565f71501549e2ba35dbd48f6f5e639d83
leoledru <43638712+leoledru@users.noreply.github.com>
2022-02-01 16:27:56 +0100

    Add files via upload

--- figA4_postprocess.m
+++ figA4_postprocess.m
@@ -1,9 +1,21 @@
 %% FIGURE A4
 
-load('mutualist_emerg_then_kill_center.mat')
+load('data/mutualist_emerg_then_kill_center.mat')
 
 %% panel a
-plot(sum(simulation_tot==3,1)./10000 .* 100,'k','LineWidth',2)
+Mutualist_tot(simulation_tot~=3) = NaN;
+simulation_tot(Mutualist_tot<0.475) = 2;
+simulation_tot(Mutualist_tot>0.475) = 3;
+mut_percent = sum(simulation_tot==3,1)./10000 .* 100;
+
+plot(mut_percent,'k','LineWidth',2)
+xlabel('time','interpreter','latex','FontSize',20)
+ylabel('percentage of mutualistic symbionts','interpreter','latex','FontSize',20)
+xlim([0 20000])
+%% smooth panel a
+mut_percent_smooth = movmean(mut_percent,1);
+
+plot(mut_percent_smooth,'k','LineWidth',2)
 xlabel('time','interpreter','latex','FontSize',20)
 ylabel('percentage of mutualistic symbionts','interpreter','latex','FontSize',20)
 xlim([0 20000])
